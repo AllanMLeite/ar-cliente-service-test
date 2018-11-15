7c24cb780be9a0980e834513ccef0a59e72b4d29
allan <allanmleite@hotmail.com>
2018-11-15 17:43:38 -0200

    Estrutura inicial

---
+++ src/test/java/com/ar/clienteservicetest/ClienteServiceTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ar.clienteservicetest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClienteServiceTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
